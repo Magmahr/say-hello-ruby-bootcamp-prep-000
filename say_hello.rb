7336007200bf0fca54b135fd569e6d6aaffcbacd
Matt Maher <mattmaher0@gmail.com>
2018-04-26 19:22:49 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,7 @@
-def say_hello(name, name="Ruby programmer")
-  print "Hello #{name}!"
+def say_hello(kent, name="Ruby programmer")
+  if say_hello(kent)
+    print "Hello #{name}!"
+  elsif say_hello(name)
+    print "Hello #{kent}!"
+  end
 end  
